1b25ca27c31fec0298e532c5460815e5b07a0ada
Ice <usermail>
2024-11-19 07:14:45 +0000

    commting changes to pom.xml and helloapplication.java

--- src/main/java/com/example/hellocd/HellocdApplication.java
+++ src/main/java/com/example/hellocd/HellocdApplication.java
@@ -1,13 +1,25 @@
 package com.example.hellocd;
-
+  
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HellocdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HellocdApplication.class, args);
-	}
+        public static void main(String[] args) {
+                SpringApplication.run(HellocdApplication.class, args);
+        }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
